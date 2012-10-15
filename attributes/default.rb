e6f0b5fe2b603acd6bed6cdc0782611e8dfa9949
David Wittman <david@wittman.com>
2012-10-15 01:32:30 -0500

    Clean up attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,14 @@
-default[:awwbomb][:username] = "aww"
-default[:awwbomb][:home] = "/home/aww"
-default[:awwbomb][:project_home] = "/home/aww/awwbomb"
-default[:awwbomb][:dependencies] = %w{git-core memcached libcurl4-openssl-dev libssl-dev zlib1g-dev build-essential}
-default[:awwbomb][:repo] = "git://github.com/DavidWittman/awwbomb.git"
-default[:awwbomb][:port] = 4567
-default[:awwbomb][:ENV] = {
-  "CF_USERNAME" => "example",
-  "CF_APIKEY" => "35d14efa640bb7f8d0073d3cf3a777ff",
-  "CF_CONTAINER" => "awwbomb",
-  "RACK_ENV" => "production"
+default[:awwbomb] = {
+  :username => "aww",
+  :home => "/home/aww",
+  :project_home => "/home/aww/awwbomb",
+  :dependencies => %w{git-core memcached libcurl4-openssl-dev libssl-dev zlib1g-dev build-essential},
+  :repo => "git://github.com/DavidWittman/awwbomb.git",
+  :port => 4567,
+  :ENV => {
+    "CF_USERNAME" => "example",
+    "CF_APIKEY" => "35d14efa640bb7f8d0073d3cf3a777ff",
+    "CF_CONTAINER" => "awwbomb",
+    "RACK_ENV" => "production"
+  }
 }
