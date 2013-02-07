ed9677111ceebb9e6350267babf6b44da6496216
David Wittman <david.wittman@rackspace.com>
2013-02-06 20:30:11 -0600

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer_email "david@wittman.com"
 license          "WTFPL"
 description      "Installs/Configures awwbomb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 depends "firewall"
+
+supports 'ubuntu', ">= 12.04"
